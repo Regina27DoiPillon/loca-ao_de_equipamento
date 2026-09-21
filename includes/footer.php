9c1ec8d610b9ef8c2c2ca851ebd6fa71c00bb647
Regina Doi Pillon <regina123doipillon@gmail.com>
2026-09-21 19:00:25 -0300

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,40 @@
 <script src="<?php echo url; ?>decoracao/javascript.js"></script>
-    
+    <footer class="flex column meio-centro altura-centro space-between">
+        <div class="flex space-between width90">
+            <div class="flex column meio-centro altura-centro gap30">
+                <h3>Catálogo</h3>
+                <br>
+                <ul class="flex column meio-centro altura-centro gap10">
+                    <li class="lista">Andaimes e Escoras</li>
+                    <li class="lista">Geradores e Compressores</li>
+                    <li class="lista">Compactadores de Solo</li>
+                    <li class="lista">Ferramentas Elétricas</li>
+                </ul>
+            </div>
+            <div class="flex column meio-centro altura-centro gap30">
+                <h3>Institucional</h3>
+                <br>
+                <ul class="flex column meio-centro altura-centro gap10">
+                    <li class="lista">Quem Somos</li>
+                    <li class="lista">Como Alugar</li>
+                    <li class="lista">Termos de Locação</li>
+                    <li class="lista">Trabalhe Conosco</li>
+                </ul>
+            </div>
+            <div class="flex column meio-centro altura-centro gap30">
+                <h3>Contato&Suporte</h3>
+                <br>
+                <ul class="flex column meio-centro altura-centro gap10">
+                    <li class="lista">(12) 34567-8910</li>
+                    <li class="lista">Whatsapp</li>
+                    <li class="lista">locacao.equip.manutencao@gmail.com</li>
+                    <li class="lista">Av. Six Seven, 67 - Aura</li>
+                </ul>
+            </div>
+            
+        </div>
+        <p>© 2026 Loucadora de Equipamentos - CNPJ: 00.000.000/0001-00. Todos os direitos reservados.</p>
+    </footer>
 </body>
 
 </html>
